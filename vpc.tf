470e15ce6ff67107faacfacae5b7b11de8d385e8
Michael Kimball <mkimball.it@gmail.com>
2020-06-10 18:19:34 +0000

    please vpc

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,29 @@
 module "vpc" {
-  source  = "app.terraform.io/MKimball-training/vpc/aws"
-  version = "2.39.0"
-  
-  name = "my-vpc"
+  source = "../../"
+
+  name = "simple-example"
+
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_ipv6 = true
+
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
 
   tags = {
-    Terraform = "true"
+    Owner       = "user"
     Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
   }
 }
