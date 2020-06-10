ab51c0f9f30438604ed27f2902bbb285eca8a6eb
Michael Kimball <mkimball.it@gmail.com>
2020-06-10 18:11:10 +0000

    changing vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
+  source  = "app.terraform.io/MKimball-training/vpc/aws"
+  version = "2.39.0"
+  
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
@@ -15,4 +16,9 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+module "vpc" {
+  
+  version = "2.39.0"
 }
